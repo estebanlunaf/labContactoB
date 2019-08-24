ceb0af4bb5cee7277845b41bd014e4826f310b62
estebanlunaf <estebanlunafleig@hotmail.com>
2019-08-24 08:42:12 -0400

    Iniciando web api

---
+++ contacto/apiContact/Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace apiContact.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook
+    }
+
+    public class Contact
+    {
+        [Key]
+        public int ContactID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact Type { get; set; }
+        [Required]
+        [Display(Description = "Contact")]
+        public string ContactValue { get; set; }
+    }
+}
