3e40f4466ecad45116a9b1662e81a92b2713a8af
estebanlunaf <estebanlunafleig@hotmail.com>
2019-09-20 21:25:50 -0400

    con api service

---
+++ contacto/appContacto/appContacto/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appContacto.Models
+{
+  
+
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook
+    }
+
+    public class Contact
+    {
+
+        public int ContactID { get; set; }
+        public string Name { get; set; }
+        public TypeContact Type { get; set; }
+        public string ContactValue { get; set; }
+    }
+}
