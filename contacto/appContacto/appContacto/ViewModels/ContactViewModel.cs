63c3017dc5f50fbd6c7e8b8a25c6ae3cd9103f15
estebanlf <estebanlunafleig@hotmail.com>
2019-10-11 20:01:12 -0400

    MVVM fase2

---
+++ contacto/appContacto/appContacto/ViewModels/ContactViewModel.cs
@@ -0,0 +1,11 @@
+﻿using appContacto.Services;
+
+namespace appContacto.ViewModels
+{
+    public class ContactViewModel:BaseViewModel 
+    {
+        #region Attributes
+        ApiService apiservice;
+        #endregion
+    }
+}
